4df12510a0f6efc5a8c677c4323dba9b4b458cee
近田陽佑 <chiyou@kmy04MacBook-Air.local>
2020-02-10 19:43:05 +0900

    pc用のレイアウトに調整

--- index.php
+++ index.php
@@ -27,49 +27,51 @@ $supers = $db->query('SELECT supermarket FROM kitchen GROUP BY supermarket');
   </header>
 
   <!-- 一覧 -->
-  <!-- supermarket 呼び出し  -->
-  <?php while ($super = $supers->fetch()) : ?>
-    <div class="super_details">
-      <div class="super_box">
-        <p class="super_name"><?php print($super['supermarket']); ?></p>
-      </div>
-      <!-- type　呼び出し -->
-      <?php if ($super['supermarket'] !== '') : ?>
-        <?php
-        $types = $db->prepare('SELECT type FROM kitchen WHERE supermarket=? GROUP BY type');
-        $types->execute(array(
-          $super['supermarket']
-        ));
-        ?>
-        <?php while ($type = $types->fetch()) : ?>
-          <div class="super_box">
-            <p class="super_type"><?php print($type['type']); ?></p>
-          </div>
-          <!-- name price呼び出し -->
-          <?php if ($type['type'] !== '') : ?>
-            <?php
-            $products = $db->prepare('SELECT name,TRUNCATE(AVG(price),0) FROM kitchen WHERE supermarket=? AND type=? GROUP BY name');
-            $products->execute(array(
-              $super['supermarket'],
-              $type['type']
-            ));
-            ?>
-            <div class="product_box">
-              <?php while ($product = $products->fetch()) : ?>
-                <div class="super_box product">
-                  <p><?php print($product['name']); ?></p>
-                  <p><?php print($product['TRUNCATE(AVG(price),0)']); ?>円</p>
-                </div>
-              <?php endwhile; ?>
+  <div class="index_box">
+    <!-- supermarket 呼び出し  -->
+    <?php while ($super = $supers->fetch()) : ?>
+      <div class="super_details">
+        <div class="super_box">
+          <p class="super_name"><?php print($super['supermarket']); ?></p>
+        </div>
+        <!-- type　呼び出し -->
+        <?php if ($super['supermarket'] !== '') : ?>
+          <?php
+          $types = $db->prepare('SELECT type FROM kitchen WHERE supermarket=? GROUP BY type');
+          $types->execute(array(
+            $super['supermarket']
+          ));
+          ?>
+          <?php while ($type = $types->fetch()) : ?>
+            <div class="super_box">
+              <p class="super_type"><?php print($type['type']); ?></p>
             </div>
-          <?php endif; ?>
-          <!-- name price呼び出し -->
-        <?php endwhile; ?>
-      <?php endif; ?>
-      <!-- type　呼び出し -->
-    </div>
-  <?php endwhile; ?>
-  <!-- supermarket 呼び出し  -->
+            <!-- name price呼び出し -->
+            <?php if ($type['type'] !== '') : ?>
+              <?php
+              $products = $db->prepare('SELECT name,TRUNCATE(AVG(price),0) FROM kitchen WHERE supermarket=? AND type=? GROUP BY name');
+              $products->execute(array(
+                $super['supermarket'],
+                $type['type']
+              ));
+              ?>
+              <div class="product_box">
+                <?php while ($product = $products->fetch()) : ?>
+                  <div class="super_box product">
+                    <p><?php print($product['name']); ?></p>
+                    <p><?php print($product['TRUNCATE(AVG(price),0)']); ?>円</p>
+                  </div>
+                <?php endwhile; ?>
+              </div>
+            <?php endif; ?>
+            <!-- name price呼び出し -->
+          <?php endwhile; ?>
+        <?php endif; ?>
+        <!-- type　呼び出し -->
+      </div>
+    <?php endwhile; ?>
+    <!-- supermarket 呼び出し  -->
+  </div>
 
   <!-- footer -->
   <footer>
